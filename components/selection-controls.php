4b002dba40b9488dca1bb4ef41ace82873a81c71
NZXT-DESKTOP <ave9008@gmail.com>
2018-06-03 03:49:03 +0300

    minor docs changes

--- components/selection-controls.php
+++ components/selection-controls.php
@@ -405,31 +405,31 @@ TEXT
         </div>
         <div class="row">
             <div class="col xlarge-2 large-3 medium-6">
-                <div class="mdc-list-group bg-cards example">
-                    <div class="mdc-list-item">
-                        <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
-                            <input type="hidden" value="0">
-                        </div>
-                        <div class="text">Dining</div>
+                <div class="space"></div>
+                
+                <div class="mdc-list-item">
+                    <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
+                        <input type="hidden" value="0">
                     </div>
-                    <div class="mdc-list-item">
-                        <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
-                            <input type="hidden" value="1">
-                        </div>
-                        <div class="text">Education</div>
+                    <div class="text">Dining</div>
+                </div>
+                <div class="mdc-list-item">
+                    <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
+                        <input type="hidden" value="1">
                     </div>
-                    <div class="mdc-list-item">
-                        <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
-                            <input type="hidden" value="0" disabled>
-                        </div>
-                        <div class="text">Health</div>
+                    <div class="text">Education</div>
+                </div>
+                <div class="mdc-list-item">
+                    <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
+                        <input type="hidden" value="0" disabled>
                     </div>
-                    <div class="mdc-list-item">
-                        <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
-                            <input type="hidden" value="1" disabled>
-                        </div>
-                        <div class="text">Sport</div>
+                    <div class="text">Health</div>
+                </div>
+                <div class="mdc-list-item">
+                    <div class="mdc-checkbox deep-purple-A700" tabindex="-1">
+                        <input type="hidden" value="1" disabled>
                     </div>
+                    <div class="text">Sport</div>
                 </div>
             </div>
             <div class="col xlarge-6 large-7 medium-10">
@@ -515,31 +515,31 @@ TEXT
         </div>
         <div class="row">
             <div class="col xlarge-2 large-3 medium-6">
-                <div class="mdc-list-group bg-cards example">
-                    <div class="mdc-list-item">
-                        <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
-                            <input type="radio" name="radio_3" value="val_1">
-                        </div>
-                        <div class="text">Dining</div>
+                <div class="space"></div>
+
+                <div class="mdc-list-item">
+                    <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
+                        <input type="radio" name="radio_3" value="val_1">
                     </div>
-                    <div class="mdc-list-item">
-                        <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
-                            <input type="radio" name="radio_3" value="val_1" checked>
-                        </div>
-                        <div class="text">Education</div>
+                    <div class="text">Dining</div>
+                </div>
+                <div class="mdc-list-item">
+                    <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
+                        <input type="radio" name="radio_3" value="val_1" checked>
                     </div>
-                    <div class="mdc-list-item">
-                        <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
-                            <input type="radio" name="radio_4" value="val_1" disabled>
-                        </div>
-                        <div class="text">Health</div>
+                    <div class="text">Education</div>
+                </div>
+                <div class="mdc-list-item">
+                    <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
+                        <input type="radio" name="radio_4" value="val_1" disabled>
                     </div>
-                    <div class="mdc-list-item">
-                        <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
-                            <input type="radio" name="radio_4" value="val_1" disabled checked>
-                        </div>
-                        <div class="text">Sport</div>
+                    <div class="text">Health</div>
+                </div>
+                <div class="mdc-list-item">
+                    <div class="mdc-radiobutton deep-purple-A700" tabindex="-1">
+                        <input type="radio" name="radio_4" value="val_1" disabled checked>
                     </div>
+                    <div class="text">Sport</div>
                 </div>
             </div>
             <div class="col xlarge-6 large-7 medium-10">
