fa3d15b0394bcc0cb572a5df9f87399c41913722
Hussain Almoamen <almoamen.m.h@gmail.com>
2018-07-03 13:45:35 +0300

    minor update

--- _includes/footer.php
+++ _includes/footer.php
@@ -3,7 +3,7 @@
     <div class="container">
         <div class="row">
             <div class="col xlarge-6 large-9 medium-12">
-                <p class="text-secondary">Built by <a href="https://github.com/vip9008" target="_blank">Hussain Almoamen</a> using Material Guidlines. This project is not supported or maintained by Google. Please visit <a href="https://material.io" target="_blank">material.io</a> for more information about Material Design.</p>
+                <p class="text-secondary">Built by <a class="<?= $primaryColor ?>" href="https://github.com/vip9008" target="_blank">Hussain Almoamen</a> using Material Guidlines. This project is not supported or maintained by Google. Please visit <a class="<?= $primaryColor ?>" href="https://material.io" target="_blank">material.io</a> for more information about Material Design.</p>
 
                 <p><a class="text-primary" href="https://github.com/vip9008/MDC" target="_blank">Github</a> / <a class="text-primary" href="https://github.com/vip9008/yii2-MDC" target="_blank">yii2-MDC</a></p>
             </div>
