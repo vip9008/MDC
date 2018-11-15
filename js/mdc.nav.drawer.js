cf523210c72b6ca5919af71de3d8c881c236b002
Hussain Almoamen <ave9008@gmail.com>
2018-11-15 15:15:36 +0300

    fix nav nanoScorller

--- js/mdc.nav.drawer.js
+++ js/mdc.nav.drawer.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
         $('#mdc-nav-drawer').removeClass('active');
     });
 
-    $('body').on(md_click_event, '#mdc-nav-drawer.nano .mdc-list-group.collapsed > .mdc-list-item', function(event) {
+    $('body').on(md_click_event, '#mdc-nav-drawer > .nano .mdc-list-group.collapsed > .mdc-list-item', function(event) {
         setTimeout(function() {
             nano_scoller_init("#mdc-nav-drawer.nano");
         }, 300);
     });
 
-    $('body').on(md_click_event, '#mdc-nav-drawer.nano .mdc-list-group.expanded > .mdc-list-item', function(event) {
+    $('body').on(md_click_event, '#mdc-nav-drawer > .nano .mdc-list-group.expanded > .mdc-list-item', function(event) {
         setTimeout(function() {
-            nano_scoller_init("#mdc-nav-drawer.nano");
+            nano_scoller_init("#mdc-nav-drawer > .nano");
         }, 300);
     });
 });
