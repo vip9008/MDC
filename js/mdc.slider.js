aa88f0e7cf6ba90bc960558b85b6adc03680c9c0
Hussain Almoamen <ave9008@gmail.com>
2019-11-24 15:43:21 +0300

    Fixes #3

--- js/mdc.slider.js
+++ js/mdc.slider.js
@@ -1,14 +1,40 @@
+function mdc_slider_update(selector) {
+    var min = $(selector).attr('min');
+    var max = $(selector).attr('max');
+    var val = $(selector).val();
+    var percent = (val - min) / (max - min) * 100;
+    $(selector).siblings('.highlight').width(percent + "%");
+
+    if ($(selector).parent('.mdc-slider').hasClass('discrete')) {
+        var step = 100 / ((max - min) / $(selector).attr('step'));
+        var point = 100 / percent * step;
+        $(selector).parent('.mdc-slider').css('--slider-point', step + '%');
+        $(selector).siblings('.highlight').css('background-size', point + "% 0.125rem").children('.value').text(val);
+    }
+}
+
+function mdc_slider_enable(selector) {
+    $(selector).children('input').prop('disabled', true);
+    $(selector).removeClass('disabled');
+}
+
+function mdc_slider_disable(selector) {
+    $(selector).children('input').prop('disabled', false);
+    $(selector).addClass('disabled');
+}
+
 $(document).ready(function() {
     $('.mdc-slider').each(function() {
-        var min = $(this).children('input').attr('min');
-        var max = $(this).children('input').attr('max');
-        var val = $(this).children('input').val();
-        var percent = (val - min) / (max-min) * 100;
-        $(this).children('.highlight').width(percent + "%");
-
         if ($(this).hasClass('discrete')) {
-            $(this).children('.highlight').html('<div class="value">'+val+'</div>');
+            var val = $(this).children('input').val();
+            $(this).children('.highlight').html('<div class="value">' + val + '</div>');
         }
+
+        if ($(this).children('input').prop('disabled')) {
+            $(this).addClass('disabled');
+        }
+
+        mdc_slider_update($(this).children('input'));
     });
 
     $('body').on('input', '.mdc-slider > input', function(event) {
@@ -16,14 +42,6 @@ $(document).ready(function() {
             return false;
         }
 
-        var min = $(this).attr('min');
-        var max = $(this).attr('max');
-        var val = $(this).val();
-        var percent = (val - min) / (max-min) * 100;
-        $(this).siblings('.highlight').width(percent + "%");
-
-        if ($(this).parent('.mdc-slider').hasClass('discrete')) {
-            $(this).siblings('.highlight').children('.value').text(val);
-        }
+        mdc_slider_update(this);
     });
 });
