97ac3d040fa6fe1a25c64dc2cf8c879c93cbafc7
Hussain Almomen <ave9008@gmail.com>
2021-10-07 23:28:00 +0300

    finish tabs

--- js/mdc.tabs.js
+++ js/mdc.tabs.js
@@ -1,9 +1,35 @@
 MDC.activateTab = function(tab, animate = true) {
-    if (!$(tab).hasClass('active')) {
-        $(tab).addClass('active').siblings().removeClass('active');
+    var container = $(tab).closest('.mdc-tabs');
+    var scrollable = false;
+
+    if (container.hasClass('scrollable')) {
+        container = $(tab).closest('.os-content');
+        scrollable = true;
     }
 
-    var container = $(tab).closest('.mdc-tabs');
+    if (!$(tab).hasClass('active')) {
+        $(tab).addClass('active').siblings().removeClass('active');
+
+        var tabIndex = $(tab).index();
+        var target = container.data('target');
+        if (target) {
+            target = $(target);
+
+            var direction = {
+                start: 'left',
+                end: 'right'
+            };
+
+            if (target.css('direction') == 'rtl') {
+                direction = {
+                    start: 'right',
+                    end: 'left'
+                };
+            }
+
+            target.children('.tab-page').css(direction.start, '-' + (tabIndex * 100).toString() + '%').css(direction.end, 'auto');
+        }
+    }
 
     if (animate) {
         container.addClass('animate');
@@ -12,6 +38,14 @@ MDC.activateTab = function(tab, animate = true) {
     var inkBar = container.children('.ink-bar');
     var startingSide = container.css('direction') == 'ltr' ? 'left' : 'right';
     var width = $(tab).outerWidth();
+
+    if (scrollable) {
+        $(tab).closest('.mdc-tabs.scrollable').overlayScrollbars().scroll({
+            el: $(tab),
+            scroll: 'ifneeded',
+            margin: (parseInt($('body').css('font-size')) * 2.75)
+        }, 200);
+    }
     
     if (startingSide == 'left') {
         var offset = $(tab).offset().left - container.offset().left;
@@ -35,10 +69,11 @@ MDC.updateTabs = function () {
 }
 
 jQuery(function() {
+    MDC.overlayScrollbarsInit('.mdc-tabs.scrollable');
     MDC.updateTabs();
 
     $('body').on(md_click_event, '.mdc-tabs .tab-item', function(event) {
-        if (!$(this).hasClass('active')) {
+        if (!$(this).hasClass('active') && !$(this).hasClass('disabled') && !$(this).prop('disabled')) {
             MDC.activateTab(this);
         }
     });
