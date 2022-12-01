b2867199661b1c3f171584ded807cb0ee2e49808
Hussain Almomen <ave9008@gmail.com>
2022-12-01 20:25:10 +0300

    Update mdc.tooltip.js

--- js/mdc.tooltip.js
+++ js/mdc.tooltip.js
@@ -8,32 +8,41 @@ jQuery(function() {
         // positioning the tooltip
         var baseSize = parseFloat($(':root').css('font-size'));
         var offset = $(this).offset();
-        var target = $(this).find('.mdc-tooltip');
+        var target = $(this).find('.mdc-tooltip').clone();
 
-        offset.top = offset.top + $(this).innerHeight() + baseSize - $(window).scrollTop();
-        offset.left = offset.left + ($(this).innerWidth() / 2) - ($(target).innerWidth() / 2) - $(window).scrollLeft();
-        
-        if (offset.top > ($(window).height() - $(target).innerHeight() - baseSize)) {
-            offset.top = $(window).height() - $(target).innerHeight() - baseSize;
-        }
-        if (offset.left > ($(window).width() - $(target).innerWidth() - baseSize)) {
-            offset.left = $(window).width() - $(target).innerWidth() - baseSize;
-        }
-        if (offset.left < 0) {
-            offset.left = baseSize;
-        }
+        $('body').append($(target)).ready(() => {
+            offset.top = offset.top + $(this).innerHeight() + baseSize - $(window).scrollTop();
+            offset.left = offset.left + ($(this).innerWidth() / 2) - ($(target).innerWidth() / 2) - $(window).scrollLeft();
+            
+            if (offset.top > ($(window).outerHeight() - $(target).innerHeight() - baseSize)) {
+                offset.top = $(window).outerHeight() - $(target).innerHeight() - baseSize;
+            }
+            if (offset.left > ($(window).outerWidth() - $(target).innerWidth() - baseSize)) {
+                offset.left = $(window).outerWidth() - $(target).innerWidth() - baseSize;
+            }
+            if (offset.left < 0) {
+                offset.left = baseSize;
+            }
 
-        $(target).css(offset).addClass('active');
-
-        MDC.tooltip.activeTooltip = setTimeout(function() {
-            $('.mdc-tooltip.active').removeClass('active');
-        }, (MDC.tooltip.timeout * 1000));
+            $(target).css(offset).addClass('active');
+            
+            MDC.tooltip.activeTooltip = setTimeout(() => {
+                $(target).trigger('MDC.tooltipHide').removeClass('active');
+            }, (MDC.tooltip.timeout * 1000));
+        });
     });
 
     $('body').on('mouseleave', '.has-tooltip', function(event) {
-        $('.mdc-tooltip.active').removeClass('active');
         if (typeof MDC.tooltip.activeTooltip !== 'undefined') {
             clearTimeout(MDC.tooltip.activeTooltip);
         }
+        $('.mdc-tooltip.active').trigger('MDC.tooltipHide').removeClass('active');
+    });
+
+    $('body').on('MDC.tooltipHide', '.mdc-tooltip', function(event) {
+        let tooltip = $(this);
+        setTimeout(() => {
+            $(tooltip).remove();
+        }, 400);
     });
 });
